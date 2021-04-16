95f5f66e6cb97831610a5127c5dd7ca43258166a
Mike Elsmore <mike@elsmore.me>
2021-04-16 01:49:16 +0100

    The initial act of authenicating with Twitch

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const next = require('next');
+const Bell = require('@hapi/bell');
 const Hapi = require('@hapi/hapi');
 const Debug = require('./helpers/debug');
 const { nextHandlerWrapper } = require('./next-wrapper');
@@ -16,7 +17,41 @@ const JoinMap = require('./api/joinmap');
 const AblyTokenRequest = require('./api/ablyTokenRequest');
 
 app.prepare().then(async () => {
+  // Register Bell
+  await server.register(Bell);
+  await server.auth.strategy('twitch', 'bell', {
+    provider: 'twitch',
+    password: process.env.TWITCH_COOKIE,
+    clientId: process.env.TWITCH_CLIENT_ID,
+    clientSecret: process.env.TWITCH_CLIENT_SECRET,
+    isSecure: !(process.env.NODE_ENV !== 'production'),
+    scope: ['user_read', 'channel_read'],
+  });
+
   // APIs
+  server.route({
+    method: ['GET', 'POST'], // Must handle both GET and POST
+    path: '/login', // The callback endpoint registered with the provider
+    options: {
+      auth: {
+        mode: 'try',
+        strategy: 'twitch',
+      },
+      handler(request, h) {
+        if (!request.auth.isAuthenticated) {
+          Debug.error(request.auth.error);
+          return `Authentication failed due to: ${request.auth.error.message}`;
+        }
+
+        // Perform any account lookup or registration, setup local session,
+        // and redirect to the application. The third-party credentials are
+        // stored in request.auth.credentials. Any query parameters from
+        // the initial request are passed back via request.auth.credentials.query.
+
+        return h.redirect('/game');
+      },
+    },
+  });
   server.route({
     method: 'GET',
     path: '/{user}/map',
