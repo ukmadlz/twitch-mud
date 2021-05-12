a40d30e8d91dcac39dbd663bcb50a8397ba64a73
Mike Elsmore <mike@elsmore.me>
2021-05-12 23:07:13 +0100

    Add additional logging

--- server.js
+++ server.js
@@ -19,6 +19,14 @@ const AblyTokenRequest = require('./api/ablyTokenRequest');
 const Auth = require('./api/auth');
 
 app.prepare().then(async () => {
+  await server.register({
+    plugin: require('hapi-pino'),
+    options: {
+      prettyPrint: process.env.NODE_ENV !== 'production',
+      // Redact Authorization headers, see https://getpino.io/#/docs/redaction
+      redact: ['req.headers.authorization']
+    }
+  })
   await Auth(server);
   server.route({
     method: 'GET',
